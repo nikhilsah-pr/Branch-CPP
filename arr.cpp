a438cf0569592b3a779ceed20b583b8b283a929e
nikhilsah-pr <nikhilsah236@gmail.com>
2026-02-06 17:22:11 +0530

    added all first part

--- arr.cpp
+++ arr.cpp
@@ -1,1 +1,25 @@
-#include
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    // Taking input
+    cout << "Enter " << n << " elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Displaying original array
+    cout << "\nOriginal Array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
